c1f22d1222e4da9f41d3a95476d4a7db90b78da3
Venkatesh Pulibandla <venkateshpulibandla460@gmail.com>
2022-02-15 22:54:30 +0530

    Login Page added

--- Frontend/reactapp/src/App.js
+++ Frontend/reactapp/src/App.js
@@ -1,15 +1,22 @@
 import './App.css';
 import React from "react";
-
+import Login from './Components/Login/Login';
 import HomePage from './Components/Layout/HomePage/HomePage';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
 
 
   return (
-    <div className="App">
-      <HomePage/>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path='/' element={<Login />} />
+          <Route path='/HomePage' element={<HomePage />} />
+        </Routes>
+      </div>
+    </Router>
+    
   );
 }
 
