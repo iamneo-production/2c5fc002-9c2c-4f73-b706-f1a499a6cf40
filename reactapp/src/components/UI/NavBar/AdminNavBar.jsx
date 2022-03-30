8e2f1a591e78cc5533e745cffea93d53a3905f60
LOKI <77198998+S-LOGESHWARAN@users.noreply.github.com>
2022-03-30 23:17:28 +0530

    AdminNavBar

---
+++ reactapp/src/components/UI/NavBar/AdminNavBar.jsx
@@ -0,0 +1,53 @@
+import React, { Fragment } from "react";
+
+import classes from "./NavBar.module.css";
+import { NavLink } from "react-router-dom";
+import { useAuthCxt } from "../../assests/auth-context";
+
+const AdminNavBar = (props) => {
+  const authCxt = useAuthCxt();
+  return (
+    <Fragment>
+      <nav className={classes.header} id="adminNavbar">
+        <h1 className={classes.title}>AmazePack</h1>
+        <div className={classes.navlinks}>
+          <ul>
+            <NavLink
+              to="/addProduct"
+              className={(navData) => {
+                return navData.isActive ? classes.active : undefined;
+              }}
+            >
+              <li id="adminProductButton">Products</li>
+            </NavLink>
+            <NavLink
+              to="/admin/orders"
+              className={(navData) => {
+                return navData.isActive ? classes.active : undefined;
+              }}
+            >
+              <li id="adminOrdersButton">Orders</li>
+            </NavLink>
+            <NavLink
+              to="/admin/users-list"
+              className={(navData) => {
+                return navData.isActive ? classes.active : undefined;
+              }}
+            >
+              <li id="adminViewUsersButton">View Users</li>
+            </NavLink>
+          </ul>
+        </div>
+        <button
+          id="admin-logout"
+          className={classes.logout}
+          onClick={authCxt.logoutHandler}
+        >
+          Logout
+        </button>
+      </nav>
+    </Fragment>
+  );
+};
+
+export default AdminNavBar;
