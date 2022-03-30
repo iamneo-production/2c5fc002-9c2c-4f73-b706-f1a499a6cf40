4b4010b9fe85e15bb200983420b6b69050b29364
Pavani <61678945+ThotaSivaPavani@users.noreply.github.com>
2022-03-30 22:32:38 +0530

    LoginModel

---
+++ dotnetapp/WebApp/Model/LoginModel.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Amazepack_Project.Model
+{
+    public partial class LoginModel
+    {
+        public string LoginId { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
