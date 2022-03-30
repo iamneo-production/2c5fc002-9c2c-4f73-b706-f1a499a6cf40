2f840a7f5ad19daaad57fde20736d362c260266d
Haneesh471 <97818352+Haneesh471@users.noreply.github.com>
2022-03-30 22:50:50 +0530

    Display

---
+++ reactapp/src/components/Layout/CartAndOrders/Display/Display.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import classes from "./Display.module.css";
+import Card from "../../../UI/Card";
+
+const Display = (props) => {
+  return (
+    <Card clname={classes.width}>
+      <header className={classes.header}>
+        <div id="productName" className={classes["header-div"]}>
+          Product Name
+        </div>
+        <div id="quantity" className={classes["header-div"]}>
+          Quantity
+        </div>
+        <div id="price" className={classes["header-div"]}>
+          Price
+        </div>
+        <div className={classes["header-div"]}>Options</div>
+      </header>
+      <div>{props.items}</div>
+    </Card>
+  );
+};
+
+
+export default Display;
